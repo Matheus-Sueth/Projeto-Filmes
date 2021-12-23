c0f9992dca558e58662c40168cabf8c3fa863b4c
Matheus Almeida S Mendonça <75100700+Matheus-Sueth@users.noreply.github.com>
2021-12-23 00:31:39 -0300

    Add files via upload

--- AddFilme.py
+++ AddFilme.py
@@ -1,8 +1,15 @@
 import PySimpleGUI as sg
 import sqlite3 as sql
+from os import listdir
 import re
 
 sg.theme('DarkTeal12')
+
+cam_filme = r'E:/Matheus/Filmes'
+cam_imagem = r'E:/Matheus/Imagens_Filmes'
+aux_aleatorio = []
+lista_filme = listdir(cam_filme)
+lista_imagens = listdir(cam_imagem)
 
 size_text, size_input = (10,1), (70,1)
 font_text = ('Arial',16)
@@ -12,7 +19,7 @@ cursor = banco.cursor()
 
 layout = [
     [sg.FileBrowse('FILME:', button_color='green', size=size_text,font=font_text),
-        sg.T('Aperte o botão FILME', background_color='#5A9BC4', size=size_input, key='arquivo',font=font_text)],
+        sg.Input('Aperte o botão FILME', size=size_input, key='arquivo',font=font_text, disabled=True)],
     [sg.T('')],
     [sg.T('NOTA:', justification='center', background_color='green',size=size_text, font=font_text),
         sg.Combo(lista, background_color='#5A9BC4', size=(70,7), key='nota',font=font_text)],
@@ -21,9 +28,9 @@ layout = [
         sg.Input('', background_color='#5A9BC4', size=size_input, key='genero',font=font_text)],
     [sg.T('')],
     [sg.FileBrowse('IMAGEM:', button_color='green', size=size_text, font=font_text),
-        sg.T('Aperte o botão IMAGEM', background_color='#5A9BC4', size=size_input, key='imagem',font=font_text)],
+        sg.Input('Aperte o botão IMAGEM', size=size_input, key='imagem',font=font_text, disabled=True)],
     [sg.T('')],
-    [sg.Button('VOLTAR',size=(37, 3)),sg.Button('ADICIONAR',size=(37, 3)),sg.Button('MODO RÁPIDO',size=(37, 3))],
+    [sg.Button('VOLTAR',size=(37, 3)),sg.Button('ADICIONAR',size=(37, 3)),sg.Button('PROCURAR',size=(37, 3))],
     [sg.T('')]
 ]
 
@@ -31,9 +38,9 @@ janela = sg.Window('ADICIONAR FILME', layout=layout, size=(950, 420))
 
 
 def arrumar_genero(genero_antigo):
-    aux_genero = re.split (r"[/]\s*", genero_antigo)
+    aux_genero = re.split(r"[/]\s*", genero_antigo)
     aux_genero = [palavra.capitalize() for palavra in aux_genero]
-    genero_arrumado = ['/'.join(aux_genero)]
+    genero_arrumado = '/'.join(aux_genero)
     return genero_arrumado
 
 
@@ -51,18 +58,44 @@ def adicionar(verifica=False):
             janela.hide()
             return True
 
+        if event == 'PROCURAR':
+            try:
+                cursor.execute('SELECT NOME,ANO,EXTENSÃO,IMAGEM FROM FILME')
+                valores = cursor.fetchall()
+
+                for filme in lista_filme:
+                    for valor in valores:
+                        teste = f'{valor[0]} ({valor[1]}){valor[2]}'
+                        if filme == teste:
+                            sg.popup(f'{filme} JÁ ESTÁ CADASTRADO', font=('Arial', 20))
+                            break
+                    else:
+                        imagem = re.split(r"[/.]\s*",filme)
+                        janela.Element('arquivo').Update(f'{cam_filme}/{filme}')
+                        janela.Element('imagem').Update(f'{cam_imagem}/{imagem[0]}.png')
+                        #sg.popup(f'{filme} DEVE SER CADASTRADO', font=('Arial', 20))
+                        break
+                else:
+                    sg.popup('NÃO HÁ ATUALIZAÇÕES', font=('Arial', 20))
+            except:
+                sg.popup('Erro ao Procurar Novos Filmes')
+
         if event == 'ADICIONAR':
             try:
-                if value['FILME:'] == '' or value['nota'] == '' or value['genero'] == '' or value['IMAGEM:'] == '':
+                if value['arquivo'] == '' or value['nota'] == '' or value['genero'] == '' or value['imagem'] == '':
                     sg.popup('Campos Vazios', font=('Arial', 20))
+                    continue
+
+                if value['arquivo'] == 'Aperte o botão FILME' or value['imagem'] == 'Aperte o botão IMAGEM':
+                    sg.popup('Campos Filme e/ou Imagem Vazios', font=('Arial', 20))
                     continue
 
                 if not value['nota'] in lista:
                     sg.popup('Nota desconhecida', font=('Arial',20))
                     continue
 
-                aux_filme = re.split(r"[/()]\s*",value['FILME:'])
-                aux_imagem = re.split(r"[/]\s*",value['IMAGEM:'])
+                aux_filme = re.split(r"[/()]\s*",value['arquivo'])
+                aux_imagem = re.split(r"[/]\s*",value['imagem'])
                 nome = aux_filme[3].strip()
                 ano = int(aux_filme[4])
                 extensao = aux_filme[5].strip()
@@ -81,10 +114,9 @@ def adicionar(verifica=False):
                         sg.popup(f'{nome} JÁ ESTÁ CADASTRADO', font=('Arial',20))
                         break
                 else:
-                    #cursor.execute('INSERT INTO FILME VALUES("'+str(id)+'","'+nome+'","'+str(ano)+'","'+nota+'","'+genero+'","'+imagem+'")')
-                    #banco.commit()
+                    cursor.execute(f'INSERT INTO FILME VALUES({id},"{nome}",{ano},"{extensao}","{nota}","{genero}","{imagem}")')
+                    banco.commit()
                     sg.popup('Adicionado')
-                    sg.popup('INSERT INTO FILME VALUES("'+str(id)+'","'+nome+'","'+str(ano)+'","'+extensao+'","'+nota+'","'+str(genero)+'","'+imagem+'")')
             except sql.Error as erro:
                 sg.popup(erro)
             except:
